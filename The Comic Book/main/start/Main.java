7a6cc04c9dba2a1cd68ce33f45592f4beed24a01
Ivaylo Ivanchev <lord.sparksmith@gmail.com>
2014-10-18 13:22:08 +0100

    Starting my 5th Year project with an initial commit of the folder structure + a main file that currently just says hello world.

---
+++ The Comic Book/main/start/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package start;
+
+/**
+ * @author sparksmith
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Yo, world");
+
+	}
+
+}
